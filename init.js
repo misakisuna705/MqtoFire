926d75e75325934a9b5a7bc17fd314c46596e33e
misakisuna705 <misakisuna705@gmail.com>
2019-12-18 22:33:41 +0800

    commit

--- init.js
+++ init.js
@@ -36,16 +36,46 @@ program
     });
 
     CLIENT.on("message", (tpc, msg) => {
-      const TIME = Date.now();
+      const T = Date.now();
       const MESSAGE = JSON.parse(msg.toString());
-      const DATA = { time: TIME, topic: tpc, message: MESSAGE[0] }; //MESSAGE[0] can be replaced by what you want to dump
 
-      console.log(DATA);
-      console.log("\n");
+      const PREHOUR = Number(MESSAGE[0].time.substring(11, 13)) + 8;
+      const HOUR = PREHOUR >= 24 ? PREHOUR - 24 : PREHOUR;
+      const TIME = MESSAGE[0].time.substring(0, 11) + HOUR + MESSAGE[0].time.substring(13, 19);
+      const FLAG = MESSAGE[0].data.substring(2, 4);
+      const TEMPERATURE = FLAG === "67" ? parseInt(MESSAGE[0].data.substring(4, 8), 16) * 0.1 : "null";
+      const HUMIDITY = FLAG === "67" ? parseInt(MESSAGE[0].data.substring(12, 14), 16) * 0.5 : "null";
+      const BAROMETER = FLAG === "67" ? parseInt(MESSAGE[0].data.substring(18, 22), 16) * 0.1 : "null";
+      const ACC_X = FLAG === "71" ? parseInt(MESSAGE[0].data.substring(4, 8), 16) * 0.001 : "null";
+      const ACC_Y = FLAG === "71" ? parseInt(MESSAGE[0].data.substring(8, 12), 16) * 0.001 : "null";
+      const ACC_Z = FLAG === "71" ? parseInt(MESSAGE[0].data.substring(12, 16), 16) * 0.001 : "null";
+      const LATITUDE = FLAG === "88" ? parseInt(MESSAGE[0].data.substring(4, 12), 16) * 0.0001 : "null";
+      const LONGTITUDE = FLAG === "88" ? parseInt(MESSAGE[0].data.substring(12, 20), 16) * 0.0001 : "null";
 
-      FIRESTORE.collection("MQTT")
-        .doc(TIME.toString())
-        .set(DATA);
+      const INFO = {
+        topic: tpc,
+        macaddr: MESSAGE[0].macAddr,
+        time: TIME,
+        data: MESSAGE[0].data,
+        temperature: TEMPERATURE,
+        humidity: HUMIDITY,
+        barometer: BAROMETER,
+        acc_x: ACC_X,
+        acc_y: ACC_Y,
+        acc_z: ACC_Z,
+        latitude: LATITUDE,
+        longtitude: LONGTITUDE
+      };
+
+      if (INFO.macaddr === "00000000aa44e7b3") {
+        //console.log(MESSAGE[0]);
+        console.log(INFO);
+        console.log("\n");
+
+        FIRESTORE.collection("MQTT")
+          .doc(TIME.toString())
+          .set(INFO);
+      }
     });
 
     CLIENT.on("close", () => {
